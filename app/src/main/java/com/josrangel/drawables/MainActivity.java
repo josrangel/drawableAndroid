223f693c8ef56d30909fb146023745654262f7aa
Josue Rangel <josrangel@hotmail.com>
2020-09-22 13:49:16 -0500

    Se hace el ejemplo con un arreglo de una clase abstracta que se creo(Imagen)

--- app/src/main/java/com/josrangel/drawables/MainActivity.java
+++ app/src/main/java/com/josrangel/drawables/MainActivity.java
@@ -7,8 +7,66 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.josrangel.drawables.datos.Imagen;
+import com.josrangel.drawables.datos.ImagenAbs;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    ImagenAbs[] imagenes;
+    int contador=1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.ic_barril);
+
+
+        imagenes= new ImagenAbs[]{
+                new ImagenAbs() {
+                    @Override
+                    public int retornaImagen() {
+                        return R.drawable.ic_barril;
+                    }
+                },
+                new ImagenAbs() {
+                    @Override
+                    public int retornaImagen() {
+                        return R.drawable.ic_ui_ux_design_mobile_icon_1;
+                    }
+                },
+                new ImagenAbs() {
+                    @Override
+                    public int retornaImagen() {
+                        return R.drawable.ic_ui_ux_design_mobile_icon_2;
+                    }
+                },
+                new ImagenAbs() {
+                    @Override
+                    public int retornaImagen() {
+                        return R.drawable.ic_ui_ux_design_mobile_icon_3;
+                    }
+                },
+                new ImagenAbs() {
+                    @Override
+                    public int retornaImagen() {
+                        return R.drawable.ic_ui_ux_design_mobile_icon_4;
+                    }
+                }
+        };
+    }
+
+    public void cambiaImagen(View v){
+        imageView.setImageResource(imagenes[++contador].retornaImagen());
+        if(contador==4){
+            contador=-1;
+        }
+    }
+
+    /*
+    *
     ImageView imageView;
     Imagen[] imagenes= {new Imagen(R.drawable.ic_barril), new Imagen(R.drawable.ic_ui_ux_design_mobile_icon_1), new Imagen(R.drawable.ic_ui_ux_design_mobile_icon_2),new Imagen(R.drawable.ic_ui_ux_design_mobile_icon_3),new Imagen(R.drawable.ic_ui_ux_design_mobile_icon_4)};
     int contador=1;
@@ -31,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
             contador=-1;
         }
     }
+    * */
 
     /*ImageView imageView;
     int[] imagenes= {R.drawable.ic_barril, R.drawable.ic_ui_ux_design_mobile_icon_1, R.drawable.ic_ui_ux_design_mobile_icon_2,R.drawable.ic_ui_ux_design_mobile_icon_3,R.drawable.ic_ui_ux_design_mobile_icon_4};
---
+++ app/src/main/java/com/josrangel/drawables/datos/ImagenAbs.java
@@ -0,0 +1,7 @@
+package com.josrangel.drawables.datos;
+
+public abstract class ImagenAbs {
+
+    abstract public int retornaImagen ();
+    /*abstract public int retornaImagen (int [] arreglo);*/
+}
