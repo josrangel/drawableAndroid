bc3f3bc16510e887eb762f78b505b551db0d0435
Josue Rangel <josrangel@hotmail.com>
2020-09-22 12:51:40 -0500

    Se termina el ejemplo

---
+++ app/src/main/java/com/josrangel/drawables/MainActivity.java
@@ -0,0 +1,32 @@
+package com.josrangel.drawables;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    int[] imagenes= {R.drawable.ic_barril, R.drawable.ic_ui_ux_design_mobile_icon_1, R.drawable.ic_ui_ux_design_mobile_icon_2,R.drawable.ic_ui_ux_design_mobile_icon_3,R.drawable.ic_ui_ux_design_mobile_icon_4};
+    int contador=1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.ic_barril);
+
+
+    }
+
+    public void cambiaImagen(View v){
+        imageView.setImageResource(imagenes[++contador]);
+        if(contador==4){
+            contador=-1;
+        }
+    }
+}
