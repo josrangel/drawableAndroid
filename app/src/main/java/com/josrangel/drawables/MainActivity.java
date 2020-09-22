8991f4a044432189640ec70118ca95252eb19751
Josue Rangel <josrangel@hotmail.com>
2020-09-22 13:15:16 -0500

    Se hace el ejemplo con un arreglo de un objeto que se creo(Imagen)

--- app/src/main/java/com/josrangel/drawables/MainActivity.java
+++ app/src/main/java/com/josrangel/drawables/MainActivity.java
@@ -6,8 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.josrangel.drawables.datos.Imagen;
+
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
+    Imagen[] imagenes= {new Imagen(R.drawable.ic_barril), new Imagen(R.drawable.ic_ui_ux_design_mobile_icon_1), new Imagen(R.drawable.ic_ui_ux_design_mobile_icon_2),new Imagen(R.drawable.ic_ui_ux_design_mobile_icon_3),new Imagen(R.drawable.ic_ui_ux_design_mobile_icon_4)};
+    int contador=1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.ic_barril);
+
+
+    }
+
+    public void cambiaImagen(View v){
+        imageView.setImageResource(imagenes[++contador].getImageResource());
+        if(contador==4){
+            contador=-1;
+        }
+    }
+
+    /*ImageView imageView;
     int[] imagenes= {R.drawable.ic_barril, R.drawable.ic_ui_ux_design_mobile_icon_1, R.drawable.ic_ui_ux_design_mobile_icon_2,R.drawable.ic_ui_ux_design_mobile_icon_3,R.drawable.ic_ui_ux_design_mobile_icon_4};
     int contador=1;
 
@@ -28,5 +53,5 @@ public class MainActivity extends AppCompatActivity {
         if(contador==4){
             contador=-1;
         }
-    }
+    }*/
 }
---
+++ app/src/main/java/com/josrangel/drawables/datos/Imagen.java
@@ -0,0 +1,13 @@
+package com.josrangel.drawables.datos;
+
+public class Imagen {
+    int imageResource;
+
+    public Imagen(int imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+}
